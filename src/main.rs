751615cd3644061c9667840a089074ed866bdee1
Sxlly <shaesullivan.buisness1@gmail.com>
2024-10-08 18:55:47 +0800

    changes and comments

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,11 @@
 
 fn main() {
     
+    let string1 = String::from("im a string"); //declaring a mutable
+    let string2 = &string1; //declaring a another string variable that uses &str to borrow string1's value without becoming the new owner
+
+    //this allows string 1 to still be used as string 2 only borrowed string1
+    println!("{}", string1); //print string1 to terminal
+    println!("{}", string2); //print string2 to terminal
 }
+
